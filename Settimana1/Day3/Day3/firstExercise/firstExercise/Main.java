3941feb0178ab9c0685fd3b58c8931b4c49d3744
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-06-28 15:34:54 +0200

    Commit

--- Settimana1/Day3/Day3/firstExercise/firstExercise/Main.java
+++ Settimana1/Day3/Day3/firstExercise/firstExercise/Main.java
@@ -4,18 +4,36 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Inserisci una frase");
 		String phrase = input.nextLine();
 		stringaPariDispari(phrase);
+		input.close();
 	}
-	
+
 	public static void stringaPariDispari(String stringToCheck) {
-		if (stringToCheck.length() % 2 == 0 ) {
+		if (stringToCheck.length() % 2 == 0) {
 			System.out.println(true);
 		} else {
 			System.out.println(false);
 		}
 	}
+
+//	public static boolean isAnnoBisestile(int anno) {
+//	    return anno % 400 == 0 ? true : (anno % 100 == 0 ? false : (anno % 4 == 0 ? true : false));
+//	}
+
+	public static boolean isAnnoBisestile(int anno) {
+	    if (anno % 400 == 0) {
+	        return true;
+	    } else if (anno % 100 == 0) {
+	        return false;
+	    } else if (anno % 4 == 0) {
+	        return true;
+	    } else {
+	        return false;
+	    }
+	}
+
 }
