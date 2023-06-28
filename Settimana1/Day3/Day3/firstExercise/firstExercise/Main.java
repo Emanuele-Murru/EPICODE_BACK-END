0f40a30f32ee491e01fb5eb8b6f755f777e0903c
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-06-28 15:15:16 +0200

    Ex1

--- Settimana1/Day3/Day3/firstExercise/firstExercise/Main.java
+++ Settimana1/Day3/Day3/firstExercise/firstExercise/Main.java
@@ -1,5 +1,21 @@
 package firstExercise;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Inserisci una frase");
+		String phrase = input.nextLine();
+		stringaPariDispari(phrase);
+	}
+	
+	public static void stringaPariDispari(String stringToCheck) {
+		if (stringToCheck.length() % 2 == 0 ) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
 }
