8d3e2cdc7215e525a7a767fb77284885687a2368
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-06-29 00:39:58 +0200

    Finish

--- Settimana1/Day3/Day3/thirdExercise/thridExercise/Main.java
+++ Settimana1/Day3/Day3/thirdExercise/thridExercise/Main.java
@@ -1,5 +1,40 @@
 package thridExercise;
+import java.util.Scanner;
 
 public class Main {
-	
+
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String input;
+
+		do {
+			System.out.print("Scrivi una frase qui: ");
+			input = scanner.nextLine();
+
+			if (!input.equals(":q")) {
+				suddividiStringa(input);
+			}
+		} while (!input.equals(":q"));
+
+		scanner.close();
+	}
+
+	public static void suddividiStringa(String input) {
+		
+		String[] caratteri = input.split("");
+		StringBuilder risultato = new StringBuilder();
+
+		for (String carattere : caratteri) {
+			risultato.append(carattere).append(",");
+		}
+
+		//Per rimuovere la virgola ⬇️
+		if (risultato.length() > 0) {
+			risultato.deleteCharAt(risultato.length() - 1);
+		}
+
+		System.out.println("Risultato: " + risultato.toString());
+	}
 }
