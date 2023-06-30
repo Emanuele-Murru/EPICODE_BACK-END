84fbdb84d2662a1810c8c6d7171e154036b50cc0
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-06-30 16:42:16 +0200

    Commit

--- Settimana01/ProgettoSettimana01/ProgettoSettimana01/src/Main.java
+++ Settimana01/ProgettoSettimana01/ProgettoSettimana01/src/Main.java
@@ -3,63 +3,72 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		
-		//Prova file audio
-		System.out.println("Inserisci il titolo della traccia");
-		String titoloTrack = input.nextLine();
-		
-		System.out.println("inserisci la durata della traccia");
-		int durata = input.nextInt();
-		input.nextLine();
-		
-		System.out.println("inserisci il volume");
-		int volume = input.nextInt();
-		
-		Audio firstTrack = new Audio(titoloTrack, durata, volume);
-		firstTrack.play();
-		input.nextLine();
-		
-		System.out.println("-------------------------------------");
-		
-		//Prova file immagini
-		System.out.println("Inserisci il nome dell'immagine");
-		String titoloImage = input.nextLine();
-		
-		System.out.println("Inserisci la luminosita che preferisci da 1 a 10");
-		int luminosita = input.nextInt();
-		input.nextLine();
-		
-		Immagini firstImage = new Immagini(titoloImage, luminosita);
-		firstImage.aumentaLuminosita();
-		firstImage.aumentaLuminosita();
-		firstImage.aumentaLuminosita();
-		firstImage.aumentaLuminosita();
-		firstImage.diminuisciLuminosita();
-		firstImage.show();
-		
-		System.out.println("-------------------------------------");
 
-		
-		//Prova file video
-		System.out.println("Inserisci il nome del video");
-		String titoloVideo = input.nextLine();
-		
-		System.out.println("Inserisci la luminosita che preferisci da 1 a 10");
-		int lumos = input.nextInt();
-		
-		System.out.println("inserisci il volume");
-		int volumeVideo = input.nextInt();
-		
-		Video firstVideo = new Video(titoloVideo, durata, lumos, volumeVideo);
-		firstVideo.aumentaLuminosita();
-		firstVideo.alzaVolume();
-		firstVideo.play();
-		
+		Scanner input = new Scanner(System.in);
+		Multimedia[] files = new Multimedia[5];
+
+		for (int i = 0; i < files.length; i++) {
+			System.out.println("Inserisci il titolo del file " + (i + 1) + ":");
+			String title = input.nextLine();
+			System.out.println("Inserisci il genere di file: \n 1)Audio; \n 2)Immagine; \n 3)Video;");
+			int genre = Integer.parseInt(input.nextLine()); // Trasformo l'input da stringa a numero
+
+			switch (genre) {
+
+			case 1:
+
+				System.out.println("Inserisci il volume (con un numero da 1 a 10): ");
+				int volumeAudio = Integer.parseInt(input.nextLine());
+
+				System.out.println("Inserisci la durata: ");
+				int durataAudio = Integer.parseInt(input.nextLine());
+
+				files[i] = new Audio(title, volumeAudio, durataAudio);
+
+				System.out.println(" \n ------------------------------------------------------ \n");
+
+				break;
+
+			case 2:
+
+				System.out.println("Inserisci la luminosita(con un numero da 1 a 10) : ");
+				int luminosita = Integer.parseInt(input.nextLine());
+
+				files[i] = new Immagini(title, luminosita);
+
+				System.out.println(" \n ------------------------------------------------------ \n");
+
+				break;
+
+			case 3:
+
+				System.out.println("Inserisci la luminosita(con un numero da 1 a 10) : ");
+				int luminositaVideo = Integer.parseInt(input.nextLine());
+
+				System.out.println("Inserisci il volume (con un numero da 1 a 10): ");
+				int volumeVideo = Integer.parseInt(input.nextLine());
+
+				System.out.println("Inserisci la durata: ");
+				int durataVideo = Integer.parseInt(input.nextLine());
+
+				files[i] = new Video(title, luminositaVideo, volumeVideo, durataVideo);
+
+				System.out.println(" \n ------------------------------------------------------ \n");
+
+				break;
+
+			default:
+				System.out.println("Genere non valido!! Scegli tra i generi 1 - 2 - 3.");
+				i--;
+				
+				System.out.println(" \n ------------------------------------------------------");
+
+				break;
+			}
+
+		}
+
 		input.close();
-		
-		
 	}
 
 }
